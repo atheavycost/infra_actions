b2bff0b0ca29350c18c7309a788c0cd833a4c766
Maslow Victor <maslowik@yandex.ru>
2021-07-16 13:32:35 +0300

    change main.yml, and tests

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web:8000', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web:8000', 'web:5000', '5000:5000', '5000', '127.0.0.1:5000', '84.201.169.143', ]
 
 
 # Application definition
